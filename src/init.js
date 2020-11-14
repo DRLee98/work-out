94cc764045b8492c203a01260162ae365ef1bcd2
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-11-14 12:07:37 +0900

    init babel/polyfill add

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
@@ -10,7 +11,6 @@ import "./models/WorkOut";
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () =>
-  console.log(`✅  Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
